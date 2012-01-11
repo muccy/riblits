86d04ec1fc195ee605a267e60d750c6740d10348
Muccy <muccymac@gmail.com>
2012-01-11 19:28:57 +0100

    Disabled sessions; added reloading for models

--- app.rb
+++ app.rb
@@ -2,15 +2,13 @@ require 'sinatra'
 require "sinatra/reloader"
 
 class MyApp < Sinatra::Application
-	enable :sessions
-
 	configure :production do
 		set :clean_trace, true
 	end
 
 	configure :development do
     register Sinatra::Reloader
-    # also_reload '/path/to/some/file'
+    also_reload 'models/*.rb'
     # dont_reload '/path/to/other/file'
 	end
 
